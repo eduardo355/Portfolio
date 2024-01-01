76fd393b362e073023808527803a5db5a764b9fe
couch355 <tovareduardo75@gmail.com>
2023-12-31 19:50:28 -0600

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'css/Nav.css',
-        'css/AboutMe.css',
-        'css/ContactMe.css',
-        'css/Footer.css',
-        'css/Projects.css',
-        'css/Skill.css',
-        'css/Welcome.css'
-      ], // Ajusta esta línea según tu estructura de carpetas
-    },
-  },
+  plugins: [react()]
 });
