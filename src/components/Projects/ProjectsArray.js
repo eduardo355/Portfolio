4f44f4d62384f1f2633e7bf825373a1928acbc93
couch355 <ingeniero.eduardo355@gmail.com>
2024-03-17 00:41:49 -0600

    New project

--- src/components/Projects/ProjectsArray.js
+++ src/components/Projects/ProjectsArray.js
@@ -3,6 +3,14 @@
 const PROJECTS = [
     {
         id: 1,
+        name: "CHAT EN TIEMPO REAL",
+        descripcion: "Es una aplicación web de chat en tiempo real desarrollada con Vite, React y webSocket-client. Permite a los usuarios comunicarse instantáneamente en una sala de chat publica. Con una interfaz intuitiva y notificaciones en tiempo real, ofrece una experiencia de chat fluida y emocionante para usuarios de todas las edades.",
+        img: "../chatTimeReal.png",
+        urlPage: "https://chat-time-real-front.vercel.app/",
+        git: "https://github.com/eduardo355/Chat-time-real-front.git"
+    },
+    {
+        id: 2,
         name: "CALCULADORA",
         descripcion: "Desarrollé una calculadora interactiva utilizando las tecnologías modernas de React y Vite. Esta calculadora no solo realiza operaciones matemáticas básicas como suma, resta, multiplicación y división, sino que también destaca por su diseño intuitivo y eficiencia en el desarrollo.",
         img: "../CALCULATOR.png",
@@ -10,7 +18,7 @@ const PROJECTS = [
         git: "https://github.com/eduardo355/Calculator-v1.git"
     },
     {
-        id: 2,
+        id: 3,
         name: "POKEDEX",
         descripcion: "Desarrollé una aplicación web de PokeDex utilizando Vite y la PokeAPI para obtener datos de Pokémon.La aplicación ofrece una experiencia de usuario fluida al explorar y buscar Pokémon, con un diseño moderno y carga dinámica.Los usuarios pueden acceder fácilmente a información detallada de los Pokémon en una interfaz sencilla y receptiva.",
         img: "../pokeApi.png",
@@ -18,7 +26,7 @@ const PROJECTS = [
         git: "https://github.com/eduardo355/pokeApi.git"
     },
     {
-        id: 3,
+        id: 4,
         name: "TODO",
         descripcion: "Cree una sencilla aplicación TODO utilizando HTML y JavaScript. Los usuarios pueden agregar, marcar como completadas y eliminar tareas. La interfaz es intuitiva, permitiendo una gestión eficiente de tareas pendientes. La aplicación se destaca por su simplicidad y facilidad de uso, brindando una solución rápida para organizar tareas diarias.",
         img: "../TODO.png",
@@ -26,7 +34,7 @@ const PROJECTS = [
         git: "https://github.com/eduardo355/To-Do.git"
     },
     {
-        id: 4,
+        id: 5,
         name: "RICK AND MORTY",
         descripcion: "Rick and Morty Explorer es una aplicación web desarrollada con React y Vite que permite a los usuarios explorar información sobre los personajes de la famosa serie de televisión Rick and Morty. La aplicación utiliza la API pública de Rick and Morty para obtener datos actualizados sobre los personajes.",
         img: "../RickAndMorty.png",
